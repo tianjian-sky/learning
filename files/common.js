8023ac98ea2dc7641bef37bf3b3e4f1e96f6b98f
tianjian-sky <532861047@qq.com>
2018-04-07 14:14:20 +0800

    performance api

---
+++ files/common.js
@@ -0,0 +1,22 @@
+// 设置dpr
+setDpr(window)
+function setDpr (win) {
+    var dpr = 0
+    var isAndroid = win.navigator.appVersion.match(/android/gi);
+    var isIPhone = win.navigator.appVersion.match(/iphone/gi);
+    var devicePixelRatio = win.devicePixelRatio;
+    if (isIPhone) {
+        // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
+        if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {                
+            dpr = 3;
+        } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)){
+            dpr = 2;
+        } else {
+            dpr = 1;
+        }
+    } else {
+        // 其他设备下，仍旧使用1倍的方案
+        dpr = 1;
+    }
+    document.documentElement.setAttribute('data-dpr', dpr);
+}
